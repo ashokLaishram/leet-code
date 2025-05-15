8fbfaaf3f243e7afbd93cbb0bad46fc11026ebb7
nit-mhealth <mhealthnitmanipur@gmail.com>
2025-05-15 12:10:21 +0530

    day(14): fibonacci series

---
+++ day-14-fibonacci-series.ts
@@ -0,0 +1,7 @@
+function fib(n: number, memo = new Map<number, number>()): number {
+  if (n <= 1) return n;
+  if (memo.has(n)) return memo.get(n)!;
+  const result = fib(n - 1, memo) + fib(n - 2, memo);
+  memo.set(n, result);
+  return result;
+}
